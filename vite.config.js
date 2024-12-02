adced7a352a3bd93b84e2a234222f93adb2c4bb3
cairuihao <cairuihao@shouqianba.com>
2024-12-02 17:27:09 +0800

    feat: 打包配置更新

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,15 @@ export default defineConfig({
       "@": resolve(__dirname, "src"), // '@' 映射到 src 目录
     },
   },
+  server: {
+    // 修改开发服务器的入口文件路径
+    open: "/template.html", // 在浏览器中打开 template.html
+    port: 3000, // 设置默认端口为 3000
+  },
+  build: {
+    rollupOptions: {
+      input: "template.html", // 更新后的入口文件
+      main: "template.html", // 指定新的入口文件
+    },
+  },
 });
